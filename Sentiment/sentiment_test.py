76bc59ea3b384a7a0577c265ec78bafa8b2a678f
kittensighting <45043185+kittensighting@users.noreply.github.com>
2018-11-14 10:14:33 -0600

    Add files via upload

---
+++ Sentiment/sentiment_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 16:32:24 2018
+
+@author: Sasha
+"""
+#import sys
+#sys.path.append('C:/Users/Sasha/')
+import sentiment_mod as s
+
+print(s.sentiment("This makes me very happy. I love it. It is great."))
